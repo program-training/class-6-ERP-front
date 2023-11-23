e776ce03a254451f75e2f35b986c31a91b42841f
Your Name <you@example.com>
2023-11-23 23:48:16 +0200

    h

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -12,18 +12,40 @@ import { useNavigate } from "react-router-dom";
 
 // Loading Component
 const Loading: React.FC = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-    }}
-  >
-    <p>Loading...</p>
-  </div>
-);
-
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <div
+        style={{
+          border: '8px solid #f3f3f3',
+          borderTop: '8px solid #3498db',
+          borderRadius: '50%',
+          width: '50px',
+          height: '50px',
+          animation: 'spin 1s linear infinite',
+        }}
+      ></div>
+      <p style={{ marginTop: '20px', fontSize: '18px', color: '#555' }}>
+        Loading...
+      </p>
+  
+      <style>
+        {`
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+      `}
+      </style>
+    </div>
+  );
+  
 export interface ShopProductInterface {
   "product.product_id"?: string;
   "product.name": string;
