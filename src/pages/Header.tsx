14e5d722aad213a4061fabfa429cebdf5483d37c
Your Name <you@example.com>
2023-11-27 13:38:11 +0200

    g

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const Header = () => {
-  const nav = useNavigate()
+  const navigate = useNavigate();
+
+  const handleTitleClick = () => {
+    navigate('/products');
+  };
+
   return (
-    <div style={headerStyle}>
-      <h1 onClick={() =>{
-        nav('/products')
-      }}>Inventory Management</h1>
-    </div>
+    <Box sx={headerStyle} onClick={handleTitleClick}>
+      <Typography variant="h4">Inventory Management</Typography>
+    </Box>
   );
 };
 
 // Styles
 const headerStyle: React.CSSProperties = {
-  background: 'grey', // Use your preferred color
+  backgroundColor: 'grey.A700', // MUI color
   color: '#fff',
   padding: '1rem',
   textAlign: 'center',
+  cursor: 'pointer',
 };
 
 export default Header;
