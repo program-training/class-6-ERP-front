0cb70f465f21905da1c5c911269c4a715291360c
Nehorai Yaphe <yn171200@gmail.com>
2023-11-30 14:06:13 +0200

    fixt

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,18 @@ import AddProduct from "./components/AddProduct";
 import Product from "./components/Product";
 import EditProduct from "./components/EditProduct";
 
-
-
 function App() {
-
-
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        <Route index element={<Home/>} />
-        <Route path="/Sign_up" element={<Sign_up />} />
+          <Route index element={<Home />} />
+          <Route path="/Sign_up" element={<Sign_up />} />
+
+       
 
           <Route path="/" element={<Layout />}>
-          <Route path="/Products" element={<Products />} />
+            <Route path="/Products" element={<Products />} />
 
             <Route path="/Product/:id" element={<Product />} />
             <Route path="/AddProduct" element={<AddProduct />} />
--- src/components/Sign_up.test.tsx
+++ src/components/Sign_up.test.tsx
@@ -1,26 +1,50 @@
-import { expect, test } from "vitest";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import {expect, test} from 'vitest';
 import SignUp from "./Sign_up";
-import { render, screen, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom';
 
-test("Password And Confirm Password is ecooil", () => {
+test("renders SignUp component", () => {
+  render(<SignUp />);
+  const emailInput = screen.getByLabelText(/enter your email/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const confirmPasswordInput = screen.getByLabelText(/password confirmation/i);
+  const signUpButton = screen.getByRole("button", { name: /sign up/i });
+
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+  expect(confirmPasswordInput).toBeInTheDocument();
+  expect(signUpButton).toBeInTheDocument();
+});
+
+test("submits the form and displays success message", async () => {
   render(<SignUp />);
 
- 
-  const userEmail = "test@example.com";
-  const password = "password123";
+  const emailInput = screen.getByLabelText(/enter your email/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const confirmPasswordInput = screen.getByLabelText(/password confirmation/i);
+  const signUpButton = screen.getByRole("button", { name: /sign up/i });
 
+  fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+  fireEvent.change(passwordInput, { target: { value: "password123" } });
+  fireEvent.change(confirmPasswordInput, { target: { value: "password123" } });
 
-  const emailInput = screen.getByLabelText("Enter Your Email") as HTMLInputElement;
-  const passwordInput = screen.getByLabelText("Password") as HTMLInputElement;
-  const confirmPasswordInput = screen.getByLabelText("Confirm Password") as HTMLInputElement;
+  fireEvent.click(signUpButton);
 
-
-  fireEvent.change(emailInput, { target: { value: userEmail } });
-  fireEvent.change(passwordInput, { target: { value: password } });
-  fireEvent.change(confirmPasswordInput, { target: { value: password } });
-
-  
-  expect(emailInput.value).toBe(userEmail);
-  expect(passwordInput.value).toBe(password);
-  expect(confirmPasswordInput.value).toBe(password);
+  await waitFor(() => {
+    const successMessage = screen.getByText(/registration successful/i);
+    expect(successMessage).toBeInTheDocument();
+  });
 });
+
+// import { render, screen, fireEvent } from "@testing-library/react";
+// import { test, describe, expect,it } from "vitest";
+// import SignUp from "./Sign_up";
+
+// describe('SignUp Component', () => {
+//   it('renders the SignUp component', () => {
+//     const { getByText, getByLabelText } = render(<SignUp />);
+
+//     // Example: Assert that a specific text or input label is present
+//     expect(getByText('Sign Up')).not.toBeNull();
+//     expect(getByLabelText(/Enter Your Email/i)).not.toBeNull();
+//   })});
